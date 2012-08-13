28f74d305b1ce00c396800ac9ae3480b1355b209
arbabenko <arbabenko@yandex-team.ru>
2012-08-13 19:34:31 +0400

    new docs

--- search/all_6d.js
+++ search/all_6d.js
@@ -8,7 +8,7 @@ var searchData=
   ['merger_5finit_5ftime',['merger_init_time',['../class_perf_tester.html#a2d7900fd70ac05d8652fbe417f884b36',1,'PerfTester']]],
   ['metainfo_5ffile',['metainfo_file',['../indexer__launcher_8cpp.html#afc9df9a336511af846444517e13b2104',1,'indexer_launcher.cpp']]],
   ['mode',['mode',['../indexer__launcher_8cpp.html#a8e661134b3c242fa8ef7cc289ea0d28b',1,'mode():&#160;indexer_launcher.cpp'],['../searcher__tester_8cpp.html#a8e661134b3c242fa8ef7cc289ea0d28b',1,'mode():&#160;searcher_tester.cpp']]],
-  ['multiindex',['MultiIndex',['../struct_multi_index.html',1,'MultiIndex&lt; Record &gt;'],['../struct_multiindex.html',1,'Multiindex'],['../struct_multi_index.html#a0cd4f7c8a5ad407707dfc9d003101d7d',1,'MultiIndex::multiindex()']]],
+  ['multiindex',['MultiIndex',['../struct_multi_index.html',1,'MultiIndex&lt; Record &gt;'],['../struct_multi_index.html#a0cd4f7c8a5ad407707dfc9d003101d7d',1,'MultiIndex::multiindex()']]],
   ['multiindex_5f',['multiindex_',['../class_multi_indexer.html#abb208cfacdba50fd9eba35ba51cd6f1c',1,'MultiIndexer::multiindex_()'],['../class_multi_searcher.html#af44150d1c57f6eb8f65f4b724f2d3d33',1,'MultiSearcher::multiindex_()']]],
   ['multiindexer',['MultiIndexer',['../class_multi_indexer.html',1,'MultiIndexer&lt; Record &gt;'],['../class_multi_indexer.html#aaa35e6042f8904e4916141df30e6da44',1,'MultiIndexer::MultiIndexer()']]],
   ['multiplicity',['multiplicity',['../indexer__launcher_8cpp.html#a76e74046b0904e4b26f25c5fdd9f3ab6',1,'indexer_launcher.cpp']]],
--- search/classes_6d.js
+++ search/classes_6d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['multiindex',['MultiIndex',['../struct_multi_index.html',1,'MultiIndex&lt; Record &gt;'],['../struct_multiindex.html',1,'Multiindex']]],
+  ['multiindex',['MultiIndex',['../struct_multi_index.html',1,'']]],
   ['multiindexer',['MultiIndexer',['../class_multi_indexer.html',1,'']]],
   ['multisearcher',['MultiSearcher',['../class_multi_searcher.html',1,'']]],
   ['multitable',['Multitable',['../struct_multitable.html',1,'']]],
