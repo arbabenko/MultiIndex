5e15b8f170d0d6c985cbfb25cb9447358671dce6
arbabenko <arbabenko@yandex-team.ru>
2012-08-16 15:29:08 +0400

    text corrected

--- search/all_68.js
+++ search/all_68.js
@@ -4,6 +4,6 @@ var searchData=
   ['heap_5f',['heap_',['../class_ordered_lists_merger.html#a2f18b468a00fc02bf2c591e70749ad1b',1,'OrderedListsMerger']]],
   ['how_5fto_5finstall_2edox',['how_to_install.dox',['../how__to__install_8dox.html',1,'']]],
   ['how_20to_20install',['How to install',['../_how_to_install.html',1,'']]],
-  ['how_20to_20index',['How to index',['../_indexation.html',1,'']]],
-  ['how_20to_20search',['How to search',['../_search.html',1,'']]]
+  ['how_20to_20create_20a_20multi_2dindex',['How to create a multi-index',['../_indexation.html',1,'']]],
+  ['how_20to_20search_20in_20a_20multi_2dindex',['How to search in a multi-index',['../_search.html',1,'']]]
 ];
--- search/pages_68.js
+++ search/pages_68.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['how_20to_20install',['How to install',['../_how_to_install.html',1,'']]],
-  ['how_20to_20index',['How to index',['../_indexation.html',1,'']]],
-  ['how_20to_20search',['How to search',['../_search.html',1,'']]]
+  ['how_20to_20create_20a_20multi_2dindex',['How to create a multi-index',['../_indexation.html',1,'']]],
+  ['how_20to_20search_20in_20a_20multi_2dindex',['How to search in a multi-index',['../_search.html',1,'']]]
 ];
