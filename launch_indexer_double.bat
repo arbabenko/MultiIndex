afe4c3aeaa5d55e2daf4e2c58f1d33ff0f0f3d94
arbabenko <arbabenko@yandex-team.ru>
2012-08-16 17:12:40 +0400

    vocabs for sift1M

--- launch_indexer_double.bat
+++ launch_indexer_double.bat
@@ -1,15 +1,15 @@
 cd build/Release/
 indexer_launcher.exe ^
---threads_count=8 ^
+--threads_count=20 ^
 --multiplicity=2 ^
---points_file="../../sift1M.bvecs" ^
+--points_file="C:\Downloads\sift\sift_base.fvecs" ^
 --coarse_vocabs_file="../../sift1M_double_4096.dat" ^
 --fine_vocabs_file="../../sift1M_double_4096_8.dat" ^
---input_point_type="BVEC" ^
+--input_point_type="FVEC" ^
 --points_count=1000000 ^
 --space_dim=128 ^
---files_prefix="sift1M_double_16384_8" ^
---coarse_quantization_file="sift1M_double_16384_coarse_quantizations.bin" ^
+--files_prefix="sift1M_double_4096_8" ^
+--coarse_quantization_file="sift1M_double_4096_coarse_quantizations.bin" ^
 --metainfo_file="fake.txt" ^
 --use_residuals ^
 --build_coarse
--- test_searcher_double.bat
+++ test_searcher_double.bat
@@ -2,14 +2,14 @@ cd build/Release/
 searcher_tester.exe ^
 --coarse_vocabs_file="../../sift1M_double_4096.dat" ^
 --fine_vocabs_file="../../sift1M_double_4096_8.dat" ^
---query_point_type="BVEC" ^
+--query_point_type="FVEC" ^
 --use_residuals ^
 --space_dim=128 ^
---subspaces_centroids_count=256 ^
---index_files_prefix="sift1M_double_16384_8" ^
---queries_file="../../queries.bvecs" ^
---groundtruth_file="../../groundtruth.ivecs" ^
---queries_count=100 ^
+--subspaces_centroids_count=1024 ^
+--index_files_prefix="sift1M_double_4096_8" ^
+--queries_file="C:\Downloads\sift\sift_query.fvecs" ^
+--groundtruth_file="C:\Downloads\sift/sift_groundtruth.ivecs" ^
+--queries_count=10000 ^
 --neighbours_count=10000 ^
 --report_file="sift1B_16384_8_report.txt" ^
 --do_rerank
