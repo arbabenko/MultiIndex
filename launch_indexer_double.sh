bb0578821093f19d9c44a3ed7f50b8930e1d0199
arbabenko <coyangens2006@yandex.ru>
2013-10-22 11:36:48 +0400

    Minor fix

--- launch_indexer_double.sh
+++ launch_indexer_double.sh
@@ -2,15 +2,15 @@ cd build_master
 ./indexer_launcher \
 --threads_count=32 \
 --multiplicity=2 \
---points_file="/sata/ResearchData/BigAnn/bases/bigann_base.bvecs" \
---coarse_vocabs_file="sift1M_double_4096.dat" \
---fine_vocabs_file="sift1M_double_4096_8.dat" \
+--points_file="/sata/ResearchData/BigAnn/bases/sift1M.bvecs" \
+--coarse_vocabs_file="../sift1M_double_4096.dat" \
+--fine_vocabs_file="../sift1M_double_4096_8.dat" \
 --input_point_type="BVEC" \
 --points_count=1000000 \
 --space_dim=128 \
 --files_prefix="/sata/ResearchData/BigAnn/indices/sift1M_double_4096_8" \
 --coarse_quantization_file="/sata/ResearchData/BigAnn/cq/sift1M_double_4096_coarse_quantizations.bin" \
 --metainfo_file="fake.txt" \
---use_residuals
+--use_residuals \
 --build_coarse
 
--- test_searcher_double.sh
+++ test_searcher_double.sh
@@ -1,15 +1,15 @@
-cd build
+cd build_master
 ./searcher_tester \
---coarse_vocabs_file="sift1M_double_4096.dat" \
---fine_vocabs_file="sift1M_double_4096_8.dat" \
+--coarse_vocabs_file="../sift1M_double_4096.dat" \
+--fine_vocabs_file="../sift1M_double_4096_8.dat" \
 --query_point_type="BVEC" \
 --use_residuals \
 --space_dim=128 \
 --subspaces_centroids_count=1024 \
 --index_files_prefix="/sata/ResearchData/BigAnn/indices/sift1M_double_4096_8" \
 --queries_file="/sata/ResearchData/BigAnn/bases/sift1B_queries.bvecs" \
---groundtruth_file="/sata/ResearchData/BigAnn/gnd/idx_1M.ivecs" \
---queries_count=10000 \
+--groundtruth_file="/sata/ResearchData/BigAnn/gnd/sift1M_groundtruth.ivecs" \
+--queries_count=500 \
 --neighbours_count=10000 \
 --report_file="sift1M_4096_8_report.txt" \
 --do_rerank
